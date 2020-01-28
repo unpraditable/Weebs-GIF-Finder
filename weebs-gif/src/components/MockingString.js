d6cf19175b65f8d54a3ba1e59c5213db5c0e925e
Aditya Pradita <pradita397@gmail.com>
2020-01-28 16:17:35 +0700

    binding events from input and click button

--- weebs-gif/src/components/MockingString.js
+++ weebs-gif/src/components/MockingString.js
@@ -3,6 +3,14 @@ const vowels = ["a", "i", "u", "e", "o"];
 const capitalVowels = ["A", "I", "U", "E", "O"];
 
 class MockingString extends Component {
+    state = {
+        input: "",
+    }
+    handleChange = this.handleChange.bind(this);
+    handleChange(e) {
+        this.setState({input: e.target.value});
+    }
+
     mockingString = (string) => {
         let newString = ""
         for (let i = 0; i < string.length; i++) {
@@ -21,13 +29,17 @@ class MockingString extends Component {
         alert(newString);
     }
 
+    handleClick = (e) => {
+        this.mockingString(this.state.input);
+    }
+
     render() {
         return(
             <div>
                 <div>
-                    <textarea></textarea>
+                    <input type="textarea" onChange={this.handleChange}></input>
                 </div>
-                <button type="button" onClick={() => this.mockingString("Orang Jahat Adalah Orang Baik yang Disakiti")}>
+                <button type="button" onClick={this.handleClick}>
                     Mock this string
                 </button>
             </div>
